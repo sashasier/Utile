4128ef1efbd1f78feee6047d4fb9fe1ca34a7f46
Sasha Sierchuk <sashasier@gmail.com>
2017-11-04 16:56:21 -0300

    Usuario terminado

---
+++ clases/DB.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class DB
+{
+  private static $conn
+
+public static function getConn()
+  {
+    if (!self::$conn) {
+      $db = new PDO('mysql:host=localhost;dbname=utile', 'roor','root');
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      self::$conn = $db;
+    }
+    return self::$conn;
+  }
+}
+
+
+ ?>
--- clases/usuario.php
+++ clases/usuario.php
@@ -1,7 +1,7 @@
 <?php
-/**
- *
- */
+
+require_once 'DB.php';
+
 class Usuario {
 	private $id;
 	private $nombre;
@@ -15,77 +15,94 @@ class Usuario {
 		$this-> nombre = $nombre;
 		$this-> email = $email;
 		$this-> username = $username;
-		$this-> password = $this->setPassword($password);
-
-		$this->db = new PDO('mysql:host=localhost;dbname=utile', 'roor','root');
-		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$this-> password = $password;
 	}
 
-	public function setPassword($value) {
-		$this->password = password_hash($value, PASSWORD_DEFAULT);
-	}
-public function save()
+	public function save()
 {
-	$sql = 'INSERT INTO usuarios (nombre,email,username,password) VALUES (:nombre,:email,:username,:password)';
-	$stmt = $this->db->prepare($sql);
-	$stmt->bindValue(' :nombre', $this->nombre, PDO::PARAMSTR);
-	$stmt->bindValue(' :email', $this->email, PDO::PARAMSTR);
-	$stmt->bindValue(' :username', $this->username, PDO::PARAMSTR);
-	$stmt->bindValue(' :password', $this->password, PDO::PARAMSTR);
-	$stmt->execute();
+		$sql =($this->id)?$this->update():$this->insert();
+		$stmt = DB::getConn()->prepare($sql)
+		$stmt->bindValue(' :nombre', $this->nombre, PDO::PARAMSTR);
+		$stmt->bindValue(' :email', $this->email, PDO::PARAMSTR);
+		$stmt->bindValue(' :username', $this->username, PDO::PARAMSTR);
+		$stmt->bindValue(' :password', $this->password, PDO::PARAMSTR);
+		$stmt->execute();
 }
+	private function insert()
+	{
+		return ' INSERT INTO usuario (nombre, email, username, password) VALUES (:nombre, :usuario, :username, :password)';
+	}
+
+	private function update()
+	{
+		return ' UPDATE usuario SET nombre=:nombre, email=:email, username=:username,password=:password WHERE id = '.$this->id;
+	}
+
 
 	$this->email = $datos["email"];
 	$this->username = $datos["username"];
 
-public function guardarImagen() {
-	$nombre=$_FILES["avatar"]["name"];
-	$archivo=$_FILES["avatar"]["tmp_name"];
+	public function guardarImagen() {
+		$nombre=$_FILES["avatar"]["name"];
+		$archivo=$_FILES["avatar"]["tmp_name"];
 
-	$ext = pathinfo($nombre, PATHINFO_EXTENSION);
+		$ext = pathinfo($nombre, PATHINFO_EXTENSION);
 
-	$miArchivo = "img/" . $this->getEmail() . "." . $ext;
+		$miArchivo = "img/" . $this->getEmail() . "." . $ext;
 
-	move_uploaded_file($archivo, $miArchivo);
+		move_uploaded_file($archivo, $miArchivo);
 }
+	public static function find($id)
+	{
+		$sql = 'SELECT * FROM usuario WHERE id = :id';
+		$stmt = DB::getConn()->prepare($Sql);
+		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+		$stmt->execute();
 
-public function setId($id) {
-	$this->id = $id;
-}
+		$result = $stmt->fetch(PDO::FETCH_ASSOC)
 
-public function getId() {
-	return $this->id;
-}
-
-public function setEmail($email) {
-	$this->email = $email;
-}
-
-public function getEmail() {
-	return $this->email;
-}
-
-public function setPassword($password) {
-	$this->password = $password;
-}
-
-public function getPassword() {
-	return $this->password;
-}
-
-public function setUsername($username) {
-	$this->username = $username;
-}
-
-public function getUsername() {
-	return $this->username;
-}
-
-
-}
-
-?>
-
+		$usuario = new Usuario('','','', 0);
+		$usuario->toUser($result);
+		return $usuario
 	}
-	return $usuario;
+
+	public function toUser($data)
+	{
+		$this->id = $data['id'];
+		$this->nombre = $data['nombre'];
+		$this->email = $data['email'];
+		$this->username = $data['username'];
+		$this->password = $data['password'];
+	}
+
+
+	public function setId($id) {
+		$this->id = $id;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setEmail($email) {
+		$this->email = $email;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function getPassword() {
+		return $this->password;
+	}
+
+	public function setUsername($username) {
+		$this->username = $username;
+	}
+
+	public function getUsername() {
+		return $this->username;
+	}
+
 }
+?>
