ac423c67215b66a44bed7d74f2bea901f914adc1
Sasha Sierchuk <sashasier@gmail.com>
2017-11-07 11:58:50 -0300

    auth

---
+++ php/soporte.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("clases/auth.php");
+require_once("clases/validador.php");
+require_once("clases/DBMySQL.php");
+require_once("clases/DBJSON.php");
+
+$auth = new Auth();
+
+$validador = new Validador();
+
+$db = new DBMySQL();
+
+?>
